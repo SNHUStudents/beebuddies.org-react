4aa938febfc1628ce83301439a2e1c3ca9acd73e
Dayton Schuh <daytonschuh@gmail.com>
2022-03-03 13:18:43 -0800

    About The Movement section updated(also changed logo)

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -40,25 +40,6 @@ const Product = () => {
             />
           </div>
         </div>
-        <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={secondItem?.img}
-              alt={secondItem?.title}
-            />
-          </div>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-            <div className={`align-middle`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {secondItem?.title}
-              </h3>
-              <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   );
