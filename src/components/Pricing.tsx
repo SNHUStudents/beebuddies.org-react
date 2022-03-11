236db8bc22aa06703e560db685916078014c3fb8
Brandon Bolton <email@BrandonBolton.com>
2022-03-10 21:26:17 -0600

    recreated dropdown button

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -27,11 +27,6 @@ const Pricing = () => {
     { size: '3 XL', link: "https://google.com" },
     { size: '4 XL', link: "https://google.com" }
   ];
-  handleClick() {
-
-
-
-  }
 
   return (
     <section className={`bg-background py-8`} id="pricing">
@@ -188,9 +183,26 @@ const Pricing = () => {
                 {fourthPlan?.price}
                 <span className={`text-base`}> {fourthPlan?.priceDetails}</span>
               </div>
-              <div>
-                <Dropdown placeholder="Select an Size" className={`honey w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`} options={options} onClick={ this.handleClick }/>
+              <div className="p-10">
+
+                <div className="dropdown inline-block relative">
+                
+                <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+                  <span className="mr-1">Select a Size</span>
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                </button>
+                
+                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                  <li className=""><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Small</a></li>
+                  <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Medium</a></li>
+                  <li className=""><a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Large</a></li>
+                  <li className=""><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">XL</a></li>
+                  <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">2 XL</a></li>
+                  <li className=""><a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">3 XL</a></li>
+                </ul>
               </div>
+
+            </div>
             </div>
           </div>
         </div>
