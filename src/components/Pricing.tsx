5dced7186cb77a0d7d7d6602d43a8712c70ad6de
daytonschuh <daytonschuh@gmail.com>
2022-04-03 09:33:32 -0700

    Queen Bee Sold out

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -112,10 +112,13 @@ const Pricing = () => {
         </div>
         <div className={`flex flex-col w-4/6 lg:w-1/3 mx-auto rounded-lg bg-background mt-6 shadow-lg z-10`}>
           <div className={`flex-1 bg-background rounded-t rounded-b-none overflow-hidden shadow`}>
-            <div className={`p-8 text-3xl font-bold text-center`}>
+          <div className="img__wrapper">
+          <img src="http://www.savoy-sharm.com/media-room/images/hi-res/king-bed-room-accommodation-savoy-luxury-5-stars-accommodation-sharm-el-sheikh.jpg" alt="" />
+        <a className="sold_out hover:text-white">Sold out</a>
+            <div className={`p-8 text-3xl font-bold text-center opacity-50`}>
               {fourthPlan?.name}
             </div>
-            <ul className={`w-full text-center text-base font-bold`}>
+            <ul className={`w-full text-center text-base font-bold opacity-50`}>
               {fourthPlan?.features.map((feature) => (
                 <li
                   className={`border-b py-4`}
@@ -126,28 +129,29 @@ const Pricing = () => {
               ))}
             </ul>
           </div>
-            <div className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}>
+            <div className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6 opacity-50`}>
               <div className="dropdown">
                 
-                <button className="honey w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10">
+                <button className="honey w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary md:py-4 md:text-lg md:px-10 cursor-not-allowed" disabled>
                   <span className="mr-1">Select a Size</span>
-                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                  {/* <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg> */}
                 </button>
               
-                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                {/* <ul className="dropdown-menu absolute hidden text-gray-700 pt-1" aria-disabled>
                   <li><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/4gw01Zeqy0It0WkcMQ">Small</a></li>
                   <li><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/00g01Z82a2QB9sQdQV">Medium</a></li>
                   <li><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/00g8yv5U2gHr20o006">Large</a></li>
                   <li><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/dR6g0X0zIcrb8oM7sz">XL</a></li>
                   <li><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/8wM8yvciqaj3awU6ow">2 XL</a></li>
                   <li><a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="https://buy.stripe.com/dR63ebgyG9eZgVieV3">3 XL</a></li>
-                </ul>
+                </ul> */}
               </div>
 
               <div className={`w-full pt-6 text-4xl font-bold text-center`}>
                 {fourthPlan?.price}
                 <span className={`text-base`}> {fourthPlan?.priceDetails}</span>
               </div>
+            </div>
             </div>
             </div>
             <div className={`mx-auto rounded-lg bg-background mt-6 z-10`}>
